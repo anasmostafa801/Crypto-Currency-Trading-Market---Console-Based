61d6d9c8e72e6b03771f499b9e3f852c1c3c7fbe
anasmostafa801 <anasmostafa801@gmail.com>
2026-07-11 17:39:16 +0300

    The end of the Course[1]

--- lab1.cpp
+++ lab1.cpp
@@ -14,28 +14,54 @@ int getuser0ption(){
     std::cin >> user_option;
     return user_option;
 }
-int main(){
-    while(true){
-        printMenu();
-        int user0ption = getuser0ption();
+void printHelp(){
+    std::cout << "Help - your aim is to make money." << std::endl;
+    std::cout << "Analyse the market and make bids" << std::endl;
+    std::cout << "and offers. " << std::endl; 
+}
+void printMarketStats(){
+   std::cout << "Market Looks Good." << std::endl;
+}
+void enterAsk(){
+    std::cout << "Mark an offer - enter the amount " << std::endl;
+}
+void enterBid(){
+    std::cout << "Make a bid - enter the amount " << std::endl;
+}
+void printWallet(){
+    std::cout << "Your wallet is empty." << std::endl;
+}
+void gotoNextTimeframe(){
+    std::cout << "Goint to next time frame....." << std::endl; 
+}
+void processUser0ption(int user0ption){
+        if(user0ption == 0){
+            std::cout << "Invalid Option, maker another choice : " << std::endl;
+        }
         if(user0ption == 1){
             printHelp();
         }
         if(user0ption == 2){
-            exhangeStats();
+            printMarketStats();
         }
         if(user0ption == 3){
-            makeOffer();
+            enterAsk();
         }
         if(user0ption == 4){
-            makeBid();
+            enterBid();
         }
         if(user0ption == 5){
             printWallet();
         }
         if(user0ption == 6){
-            
+            gotoNextTimeframe();
         }
+}
+int main(){
+    while(true){
+        printMenu();
+        int user0ption = getuser0ption();
+        processUser0ption(user0ption);
     }
     return 0;
 }
